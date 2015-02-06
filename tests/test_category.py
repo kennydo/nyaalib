f90a3c79258ae21d8e4ecefc8514d64ab6b05b70
Kenny Do <kedo@ocf.berkeley.edu>
2015-02-05 22:02:36 -0800

    added test for invalid category values

--- tests/test_category.py
+++ tests/test_category.py
@@ -9,3 +9,8 @@ def test_reverse_lookup_of_subcategory():
 def test_values():
     assert Category.anime.value == '1_0'
     assert Category.audio__lossless_audio.value == '3_14'
+
+
+def test_lookup_invalid_values():
+    assert Category.lookup_category('not_a_real_value') is None
+    assert Category.lookup_category('1_panda') is None
