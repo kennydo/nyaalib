4ea906a4e696b6a80fd776ce41d37419aecd59ef
Kenny Do <kedo@ocf.berkeley.edu>
2015-03-06 03:05:50 -0800

    bumping version to 0.0.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='nyaalib',
-    version='0.0.7',
+    version='0.0.8',
     description='Python library for Nyaa',
     long_description=readme,
     url='https://github.com/kennydo/nyaalib',
